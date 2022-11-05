0c2b97ece88c32af5df2f820b15d2cdb1a8001b1
Dagem Tsehay <dagemtsehay4@gmail.com>
2022-11-05 16:07:57 +0300

    release files

--- src/components/footer.js
+++
@@ -1,7 +0,0 @@
-function Footer(){
-    return <div className="footer">
-        <h3>Developed By: <a href="https://dagemtsehay1.github.io/">Dagem Tsehay</a></h3>
-    </div>
-}
-
-export default Footer;
